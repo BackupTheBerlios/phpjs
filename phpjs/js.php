106b56b0e63f3b82a69aadde5f1b99fb50b08dc7
milky <milky>
2005-01-25 18:11:22 +0000

    added JSE_ flags, jse_() functions and unified js() and _exec() calls, support for declare(ticks) and jsrt::safe_ticks()

--- phpjs/js.php
+++ phpjs/js.php
@@ -1,23 +1,25 @@
-<?php define("JS_VERSION", "0.01010");  /* it's a binary version number */
+<?php define("JS_VERSION", "0.01011");  /* it's a binary version number */
 
 /*
   the javascript interpreter for php
   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-  Allows to execute PHP/JavaScript-lookalike code in a (safe) sandbox,
-  where interfaces into the hosting interpreter are possible. So it may
-  be useful to get embedded into CMS/Wiki engines, then users had the
-  ability to extend a site without having direct and full access to the
-  server resources.
+  Executes JavaScript-lookalike code safely in a sandbox. Interfaces into
+  (variables, functions) the hosting interpreter (PHP) are possible. This
+  is useful for embedding into CMS/Wiki engines to allow users to extend
+  a site without having direct and full server access.
 
-  Exceptionally this is FreeWare and not PublicDomain. (Read: you
-  can use it within and modify it for any other project, but replacing
-  this paragraph with the GNU GPL comment is not allowed.)
-  (c) 2004 WhoEver wants to. | <mario*erphesfurt·de>
+  Exceptionally this is FreeWare and not PublicDomain. (Read: you can
+  use it within and modify it for any other project, but replacing this
+  paragraph with the GNU GPL comment is not allowed [yet]. But see the
+  README.)   (c) 2004,2005 WhoEver wants to.
+
+  orig. author: <mario*erphesfurt·de>
+  enhancements: Eric Anderton <ericanderton*yahoo·com>
 
   Interfaces
   ¯¯¯¯¯¯¯¯¯¯
-   · js_exec($source_code)                 // simple all-in-one
-   · $output = js($source_code)            // alternative
+   · js_exec($source_code)                 // all-in-one, output to screen
+   · $output = js($source_code)            // alternative, uses accelerator
 
    · jsc::compile($source_code)            // compile into $bc
    · jsi::register_func($js_func_name, $php_func)
@@ -31,10 +33,11 @@ error_reporting(error_reporting() & (0xFFFF^E_NOTICE));
 
 
 #-- a few settings
-define("JS_PHPMODE", 0);   // not implemented
 define("JS_CACHE", "/tmp/js");
 define("JS_DEBUG", 0);
 define("JS_FAST_CODE", !JS_DEBUG);
+define("JS_PHPMODE", 0);   // forget it!
+define("JS_TICKMAX", 50000);  // limit execution time for interpreted scripts
 
 
 #-- language tokens (enable for more speed and less mem use)
@@ -85,63 +88,104 @@ if (JS_FAST_CODE) {
    define("JS_CASE",	44);
    define("JS_IF",	45);
    define("JS_DEFAULT",	46);
+   define("JS_VAR_STATEMENT",	47);
    define("JS_EOF",	255);
 }
 
 
+#-- easy execution interface -------------------------------------------
+define("JSE_ACCEL", 0x01);
+define("JSE_CACHE", 0x02);
+define("JSE_CLEAN", 0x08);
+define("JSE_TICKS", 0x20);
 
-
-
-
-#------------------------------------------------------------------------
-#-- simplified interface
-
-
-
-function js_exec($codestr, $cleanup=0)
+#-- simple, straight to screen
+function js_exec($script, $flags=0x0001)
 {
-    #-- parse code into global $bc
-    jsc::compile($codestr, $cleanup);
+   #-- parse code into global $bc
+   jse_compile($script, $flags);
 
-    #-- run interpreter
-    jsi::run();
-    //print_r($GLOBALS["bc"]);
-    if ($cleanup) { $GLOBALS["bc"] = NULL; }
+   #-- run interpreter
+   jse_run($flags);
+    
+   #-- clean
+   if ($flags & JSE_CLEAN) {
+      unset($GLOBALS["bc"]);
+   }
 }
 
+#-- output as return value
+function js($script="", $flags=0x0003)
+{
+   #-- start buffer
+   ob_start();
+   ob_implicit_flush(0);
 
-function js($script=NULL) {
-   global $bc;
-   $r = NULL;
+   #-- compile, exec
+   js_exec($script, $flags);
 
-   #-- compile+cache or load
-   if ($script) {
-      $md5 = JS_CACHE."/".md5($script) . ".js.bc.gz";
-      if (file_exists(JS_CACHE) && file_exists(JS_CACHE."/".$md5)) {
-         $f = gzopen($md5, "rb");
-         $bc = gzread($f, 1<<20);
-         gzclose($f);
-      }
-      else {
-         jsc::compile($script, "_CLEAN=1");
-         $r = js();
-         $f = gzopen($md5, "wb");
-         fwrite($f, serialize($bc));
-         gzclose($f);
-      }
-   }
-
-   #-- exec, collect output
-   if (!isset($r)) {
-      ob_start();
-      ob_implicit_flush(0);
-      jsi::run();
-      $r = ob_get_contents();
-      ob_end_clean();
-   }
-
+   #-- get result
+   $r = ob_get_contents();
+   ob_end_clean();
    return($r);
 }
+
+#-- compile+cache
+function jse_compile($script="", $flags) {
+   global $bc;
+
+   #-- only if script/codestring given
+   if ($script) {
+      if ($flags & JSE_CACHE) {
+         $md5 = JS_CACHE."/".md5($script) . ".js.bc.gz";
+         #-- already cached
+         if (file_exists(JS_CACHE) && file_exists(JS_CACHE."/".$md5)) {
+            if ($f = gzopen($md5, "rb")) {
+               $bc = unserialize(gzread($f, 1<<20));
+               gzclose($f);
+            }
+         }
+         #-- compile and save
+         else {
+            jsc::compile($script);
+            if (!file_exists(JS_CACHE)) { mkdir(JS_CACHE); }
+            if ($f = gzopen($md5, "wb")) {
+               fwrite($f, serialize($bc));
+               gzclose($f);
+            }
+         }
+      }
+      #-- without any caching
+      else {
+         jsc::compile($script, $flags&JSE_CLEAN);
+      }
+   }
+}
+
+#-- accelerated or in old interpreter
+function jse_run($flags=JSE_ACCEL) {
+
+   // interrupt every 1000 low-level statements
+   if ($flags & JSE_TICKS) {
+      register_tick_function(array("jsrt", "tick_safe"));
+      declare(ticks=1000);
+   }
+   
+   #-- check for accelerator/assembler presence
+   if (($flags&JSE_ACCEL) && class_exists("jsa") && class_exists("jsrt")) {
+      eval( jsa::assemble(NULL, $flags&JSE_TICKS) );
+   }
+   else {
+      jsi::run();
+   }
+   
+   #-- clean up
+   if ($flags & JSE_TICKS) {
+      unregister_tick_function(array("jsrt", "tick_safe"));
+      declare(ticks=0);
+   }
+}
+
 
 
 #-- load other parts
